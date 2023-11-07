249873f8d1b0e3eae49a6b9be5e5bbfb42ebc243
Daniel Hernandez <daniel244@live.com.mx>
2023-11-07 10:16:23 -0600

    instalando taildwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,16 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>DevStagram - @yield('titulo')</title>       
+        @vite('resources/css/app.css')
     </head>
     <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-            <a href="/contacto">Contacto</a>
-        </nav>
-
-        <h1>@yield('titulo')</h1>
+        <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
 
         <hr>
 
