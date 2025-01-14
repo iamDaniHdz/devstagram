721fa8e547886a6765ea281215f3b8f354e26615
Daniel Hernandez <daniel244@live.com.mx>
2025-01-14 12:02:06 -0600

    Crear registro con Eloquent ORM, insertar el Usuario con el Modelo

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -23,6 +24,11 @@ class RegisterController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
 
-        dd('Creando Usuario');
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
     }
 }
